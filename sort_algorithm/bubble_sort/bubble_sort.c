146d7150ed127aa82dd13947b4d452dc18daccbf
linux-downey <1148256554@qq.com>
2018-05-09 08:09:21 -0700

    bubble sort

---
+++ sort_algorithm/bubble_sort/bubble_sort.c
@@ -0,0 +1,55 @@
+#include "bubble_sort.h"
+
+
+
+
+/*atoi() conver string to int,get the dst string before the first no-number char by src string */
+int str_to_num(char *str)
+{
+    return atoi(str); 
+}
+
+
+void sort_bubble(u32 *data,u32 len)
+{
+    u32 i=0,j=0;
+    u32 temp=0;
+    for(i=0;i<len;i++)
+    {
+        for(j=0;j<len-1-i;j++)
+        {
+            if(data[j]>data[j+1])
+            {
+                temp=data[j+1];
+                data[j+1]=data[j];
+                data[j]=temp;
+            }
+        }
+    }
+}
+
+
+
+int main(int argc,char *argv[])
+{
+    u32 *sort_num_array=malloc((argc-1)*sizeof(u32));
+    if(NULL==sort_num_array) return 0;
+    memset(sort_num_array,0,(argc-1)*sizeof(u32));
+    if(argc<2)
+    {
+        printf("Too few arg!!\r\n");
+    }
+    else
+    {
+        for(int i=0;i<argc-1;i++)
+        {
+            sort_num_array[i]=str_to_num(argv[i+1]);
+        }
+        sort_bubble(sort_num_array,(argc-1));
+        for(int i=0;i<argc-1;i++)
+        {
+            printf("%d\t",sort_num_array[i]);
+        }     
+        printf("\r\n");
+    }
+}
---
+++ sort_algorithm/bubble_sort/bubble_sort.h
@@ -0,0 +1,15 @@
+#ifndef _BUBBLE_SORT_H
+#define _BUBBLE_SORT_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
+typedef int s32;
+
+
+#endif
