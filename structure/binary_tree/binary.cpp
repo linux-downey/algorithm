8376070de4543f693253bde8c82bac3b6a1b7987
linux-downey <1148256554@qq.com>
2018-06-06 05:22:27 -0700

    binary tree basic operation

--- structure/binary_tree/binary.cpp
+++ structure/binary_tree/binary.cpp
@@ -1,270 +1,35 @@
 #include "binary.h"
 #include "queue.h"
-
-
-template<typename T>
-void BinaryTree::print_node_value(T value)
-{
-	cout<<value<<"  ";
-}
-
-
-
-BinaryTree::BinaryTree()
-{
-
-}
-
-
-/*destroy the tree.
-**
-*/
-BinaryTree::~BinaryTree()
-{
-	destroy_tree(root);
-}
-
-
-
-/*
-post order traversal way to delete node one by one.
-*/
-s32 BinaryTree::destroy_tree(TREE_NODE *root)
-{
-	if(NULL==root)
-	{	
-		return -1;
-	}
-	
-	destroy_tree(root->left_child);
-	
-	destroy_tree(root->right_child);
-	
-	delete root;
-
-	root=NULL;
-	
-	return 0;
-}
-
-
-
-/*Create a binary tree and get the root,So that it can destroy the tree automatically when class call destructor function. 
-**
-*/
-TREE_NODE* BinaryTree::create_tree()
-{
-	root=create_tree_preorder();
-	return root;
-}
-
-
-/*Create tree from user input node elem.
-**
-*/
-TREE_NODE* BinaryTree::create_tree_preorder()
-{
-	TREE_NODE* root;
-	char c;
-	cin>>c;
-	if('!'==c)
-	{
-		return NULL;
-	}
-	root=new TREE_NODE;
-	root->value=c;
-	
-	root->left_child=create_tree_preorder();
-	root->right_child=create_tree_preorder();
-	
-	return root;
-}
-
-/*Get tree root.
-**
-*/
-TREE_NODE* BinaryTree::get_root()
-{
-	return root;
-}
-
-
-
-/*Create extendable null tree.
-**
-*/
-s32 BinaryTree::create_tree(ELEM_TYPE value)
-{
-	root=new TREE_NODE;
-	root->value=value;
-	root->left_child=NULL;
-	root->right_child=NULL;
-	return 0;
-}
-
-
-/*Traversal the tree in the way of preorder.
-**
-*/
-s32 BinaryTree::preorder_traversal(TREE_NODE *root)
-{
-	if(NULL==root)
-	{
-		return -1;
-	}
-	print_node_value(root->value-0x30);
-	
-	preorder_traversal(root->left_child);
-	preorder_traversal(root->right_child);
-	return 0;
-}
-
-
-/*Traversal the tree in the way of middle order.
-**
-*/
-s32 BinaryTree::middle_order_traversal(TREE_NODE *root)
-{
-	if(NULL==root)
-	{
-		return -1;
-	}
-	middle_order_traversal(root->left_child);
-	print_node_value(root->value-0x30);
-	middle_order_traversal(root->right_child);
-	return 0;
-}
-
-/*Traversal the tree in the way of post order.
-**
-*/
-s32 BinaryTree::post_order_traversal(TREE_NODE *root)
-{
-	if(NULL==root)
-	{
-		return -1;
-	}
-	post_order_traversal(root->left_child);
-	post_order_traversal(root->right_child);
-	print_node_value(root->value-0x30);
-	return 0;
-	
-}
-
-/*Traversal the tree in the way of level.
-**Implementation:1.Push the root into queue,
-**				 2.pop queue node,print node.Push two of child nodes into the queue if child node != NULL.
-**				 3.Perform step 2 till queue is empty.
-**
-**The queue value is a pointer to binary node.
-*/
-s32 BinaryTree::level_traversal(TREE_NODE *root)
-{
-	QUEUE_ELEM_TYPE elem;
-	QUEUE queue;
-	queue.push(root);
-
-	while(-1!=queue.pop(elem))
-	{
-		if(NULL!=(elem->left_child))
-		{
-			queue.push(elem->left_child);	
-		}
-		if(NULL!=(elem->right_child))
-		{
-			queue.push(elem->right_child);	
-		}
-		print_node_value(elem->value-0x30);
-	}
-	return 0;
-}
-
-/*Get the height of tree.
-**In the way of post order traversal,treat each subtree as a separate tree,get subtree's height recursively.
-*/
-u32 BinaryTree::get_height_of_tree(TREE_NODE *root)
-{
-	if(NULL==root)
-	{
-		return 0;
-	}
-	u32 left_height=get_height_of_tree(root->left_child);
-	u32 right_height=get_height_of_tree(root->right_child);
-	return left_height>right_height?left_height+1:right_height+1;
-}
-
-
-/*Get all leaves counts.Leaf definition:the node without any subnode.
-*/
-u32 BinaryTree::leaves_counts(TREE_NODE *root)
-{
-	if(NULL==root)
-	{
-		return 0;
-	}
-	if((NULL==root->left_child) && (NULL==root->right_child))
-	{
-		return 1;
-	}
-	u32 left_counts=leaves_counts(root->left_child);
-	u32 right_counts=leaves_counts(root->right_child);
-	return left_counts+right_counts;
-}
-
-
-/*Get all nodes counts. 
-**
-*/
-u32 BinaryTree::nodes_counts(TREE_NODE *root)
-{
-	if(NULL==root)
-	{
-		return 0;
-	}
-	u32 left_counts=nodes_counts(root->left_child);
-	u32 right_counts=nodes_counts(root->right_child);
-	return left_counts+right_counts+1;
-}
-
-
-/*Get nth level nodes.Root is level 0,and so on.
-**
-*/
-u32 BinaryTree::get_level_nodes(TREE_NODE *root,u32 level)
-{
-	if((NULL==root)||(level<0))
-	{
-		return 0;
-	}
-	if(0==level)
-	{
-		return 1;
-	}
-	u32 left_node_counts=get_level_nodes(root->left_child,level-1);
-	u32 right_node_counts=get_level_nodes(root->right_child,level-1);
-	return right_node_counts+left_node_counts;
-}
+#include "stack.h"
 
 
 
 
-
-#ifdef BITREE_DEBUG
+#ifdef BITREE_TEST
 
 int main()
 {
+	QUEUE<u32> queue;
+
 	u32 height=0;
 	u32 node_cts=0;
 	u32 leaf_couts=0;
 	u32 level_node_counts=0;
-	TREE_NODE* head; 
 
-	BinaryTree BT;
-	head=BT.create_tree();
+	TreeNode<ELEM_TYPE>* head;
+	BinaryTree<TreeNode<ELEM_TYPE>> BT;
+
+	 head=BT.create_tree();
 	
+	BT.preorder_traversal_nonRecursive(head);
+	cout<<endl;
 	BT.preorder_traversal(head);
 	cout<<endl;
+	BT.middle_order_traversal_nonRecursive(head);
+	cout<<endl;
 	BT.middle_order_traversal(head);
+	cout<<endl;
+	BT.post_order_traversal_nonRecursive(head);
 	cout<<endl;
 	BT.post_order_traversal(head);
 	cout<<endl;
--- structure/binary_tree/queue.h
+++ structure/binary_tree/queue.h
@@ -1,9 +1,37 @@
+//  
+//                                  _oo8oo_
+//                                 o8888888o
+//                                 88" . "88
+//                                 (| -_- |)
+//                                 0\  =  /0
+//                               ___/'==='\___
+//                             .' \\|     |// '.
+//                            / \\|||  :  |||// \
+//                           / _||||| -:- |||||_ \
+//                          |   | \\\  -  /// |   |
+//                          | \_|  ''\---/''  |_/ |
+//                          \  .-\__  '-'  __/-.  /
+//                        ___'. .'  /--.--\  '. .'___
+//                     ."" '<  '.___\_<|>_/___.'  >' "".
+//                    | | :  `- \`.:`\ _ /`:.`/ -`  : | |
+//                    \  \ `-.   \_ __\ /__ _/   .-` /  /
+//                =====`-.____`.___ \_____/ ___.`____.-`=====
+//                                  `=---=`
+// 
+//                          佛祖保佑         永无bug
+// 
+
+/**@file queue.h
+ * @brief header file
+ * @author downey
+ * @date 2018-06-03
+ * 
+ * */
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
 
 #include <iostream>
-
-#include "binary.h"
+#include <string>
 
 
 #ifndef PRE_DEFINE
@@ -12,6 +40,7 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
 using u8 =unsigned char;
 using u32=unsigned int;
@@ -32,45 +61,191 @@ struct Q_NODE
 };
 
 
-using QUEUE_ELEM_TYPE=TREE_NODE_PTR;
 
 
-using QUEUE_NODE = Q_NODE<QUEUE_ELEM_TYPE>;  //Take the binary tree node pointer as queue-node value part.
-using QUEUE_NODE_PTR = Q_NODE<QUEUE_ELEM_TYPE>*;
 
-// using QUEUE_NODE = Q_NODE<u32>;  //Take the binary tree node pointer as queue-node value part.
-// using QUEUE_NODE_PTR = Q_NODE<u32>*;
-
+template <typename T>
 struct Q
 {
-	QUEUE_NODE* head;
-	QUEUE_NODE* tail;
+	Q_NODE<T>* head;
+	Q_NODE<T>* tail;
 	u32 q_len;
 	u32 max_len;
 };
 
 
 
-
-
+template <class T>
 class QUEUE
 {
 	public:
 		~QUEUE();
 		QUEUE(u32 max_queue_len=DEFAULT_MAX_QUEUE_LEN);
 		s32 destroy_queue();
+		s32 push(T data);
+		s32 pop(T& data);
 		
-		s32 push(QUEUE_ELEM_TYPE data);
-
-		s32 pop(QUEUE_ELEM_TYPE& data);
-		
-		s32 pop(); 
+		s32 pop();
 		s32 is_empty();
 		s32 is_full();
 		s32 get_queue_len();
 	private:
-		Q queue;	
+		Q<T> queue;
 };
+
+
+
+/*Create a queue in constructor.
+**User needn't to init the queue,because the NULL-queue was created at instantiation.
+**User defines a max queue len,defualt is 255.
+*/
+template <class T>
+QUEUE<T>::QUEUE(u32 max_queue_len)
+{
+	queue.head=queue.tail=new Q_NODE<T>;
+	queue.head->next=NULL;
+	queue.max_len=max_queue_len;
+	queue.q_len=0;
+}
+
+
+/*Destroy the queue automatically in Destructor to avoid leak of memory.
+**User can also call destroy_queue() to clean the queue.Double-destroy is acceptable.
+*/
+template <class T>
+QUEUE<T>::~QUEUE()
+{
+	destroy_queue();
+}
+
+
+/*Destroy the queue,free memory.
+**
+*/
+template <class T>
+s32 QUEUE<T>::destroy_queue()
+{
+	while(!pop());
+	delete queue.head;
+	return 0;
+}
+
+
+/*Push node into queue.
+**
+*/
+template <class T>
+s32 QUEUE<T>::push(T data)
+{
+	Q_NODE<T> *node=new Q_NODE<T>;
+	if(is_full())
+	{
+		return -1;
+	}
+	node->data=data;
+	queue.tail->next=node;
+	queue.tail=node;
+	queue.q_len++;
+	return 0;
+}
+
+
+/*
+**Pop elem without output.Called in destructor.
+*/
+template <class T>
+s32 QUEUE<T>::pop()
+{
+	Q_NODE<T> *temp=NULL;
+	if(is_empty())
+	{
+		return -1;	
+	}
+	temp=queue.head->next;  //get head of queue.
+	if(temp==queue.tail)    //last node
+	{
+		queue.tail=queue.head;
+		queue.head->next=NULL;
+	}
+	else
+	{
+		queue.head->next=temp->next;  //head point to next node.
+	}
+	queue.q_len--;
+	delete temp;
+	return 0;
+}
+
+
+/*Pop elem.
+**
+*/
+template <class T>
+s32 QUEUE<T>::pop(T& data)
+{
+	Q_NODE<T> *temp=NULL;
+	if(is_empty())
+	{
+		return -1;	
+	}
+	temp=queue.head->next;  //get head of queue.
+	if(temp==queue.tail)    //last node
+	{
+		queue.tail=queue.head;
+		queue.head->next=NULL;
+	}
+	else
+	{
+		queue.head->next=temp->next;  //head point to next node.
+	}
+	data=temp->data;
+	queue.q_len--;
+	delete temp;
+	return 0;
+}
+
+
+/*Check if the queue is empty.
+**
+*/
+template <class T>
+s32 QUEUE<T>::is_empty()
+{
+	if(0==queue.q_len)
+	{
+		return -1;
+	}
+	return 0;
+}
+
+
+/*Check if the queue is full.
+**
+*/
+template <class T>
+s32 QUEUE<T>::is_full()
+{
+	if(queue.q_len>=queue.max_len)
+	{
+		//cout<<"is full!"<<endl;
+		return -1;
+	}
+	return 0;
+}
+
+
+/*Get length of queue.
+**
+*/
+template <class T>
+s32 QUEUE<T>::get_queue_len()
+{
+	return queue.q_len;
+}
+
+
+
+
 
 
 
